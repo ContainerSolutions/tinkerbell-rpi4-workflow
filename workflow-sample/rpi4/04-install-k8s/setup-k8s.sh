f1ffd3edc600215bdeb875626a47a6d475995ff8
Otto, Adam <adam.otto@container-solutions.com>
2020-05-05 16:36:47 +0200

    set interface for flannel

--- workflow-sample/rpi4/04-install-k8s/setup-k8s.sh
+++ workflow-sample/rpi4/04-install-k8s/setup-k8s.sh
@@ -7,8 +7,8 @@ secret=$3
 url=$4
 if [[ -n $secret && -z $url ]]
 then
- curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_CLUSTER_SECRET=${secret} INSTALL_K3S_EXEC="--bind-address=${ip} --advertise-address=${ip} --node-ip=${ip} --node-external-ip=${ip}" K3S_NODE_NAME=${hostname} sh - || echo "k8s master installed"
+ curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_CLUSTER_SECRET=${secret} INSTALL_K3S_EXEC="--bind-address=${ip} --advertise-address=${ip} --node-ip=${ip} --node-external-ip=${ip} --flannel-iface=eth0" K3S_NODE_NAME=${hostname} sh - || echo "k8s master installed"
 else
- curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_CLUSTER_SECRET=${secret} K3S_URL=${url} INSTALL_K3S_EXEC="--node-ip=${ip} --node-external-ip=${ip}" K3S_NODE_NAME=${hostname}  sh - || echo "k8s worker installed"
+ curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_CLUSTER_SECRET=${secret} K3S_URL=${url} INSTALL_K3S_EXEC="--node-ip=${ip} --node-external-ip=${ip} --flannel-iface=eth0" K3S_NODE_NAME=${hostname}  sh - || echo "k8s worker installed"
 fi
 
